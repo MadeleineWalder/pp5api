6f884dc4d78af73bb7e14613880fa4567f28e867
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-07-29 15:36:51 +0000

    added profiles detail view with get and put requests

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from profiles import views
 # remember to call .as_view() if its a class view
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>', views.ProfileDetail.as_view()),
 ]
