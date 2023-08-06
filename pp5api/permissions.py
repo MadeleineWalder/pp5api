74e4350fe632782ed132b61c51b3441586095430
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-08-06 12:07:31 +0000

    add likes app with model, serializers, ulrs and views

--- pp5api/permissions.py
+++ pp5api/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-# custom permission so only the owner can edit their profile
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
   def has_object_permission(self, request, view, obj):
     if request.method in permissions.SAFE_METHODS:
