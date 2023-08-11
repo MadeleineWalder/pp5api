2a2fbb8a6733cc95e5f564cc4a2803755791dde6
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-08-11 14:05:38 +0000

    fixed bad request. Final push for deployment

--- pp5api/settings.py
+++ pp5api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import re
 
 
 if os.path.exists('env.py'):
@@ -64,7 +65,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-madeleinewalder-pp5api-i1ortkj1hso.ws-eu102.gitpod.io',
-    'pp5api-a60f85b616aa.herokuapp.com'
+    os.environ.get('ALLOWED_HOST'),
 ]
 
 
@@ -107,9 +108,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
