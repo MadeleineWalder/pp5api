c9c8b43a557dad5be2e3fe0f0ebf047af10aa765
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-08-20 14:20:50 +0000

    add cors settings

--- pp5api/settings.py
+++ pp5api/settings.py
@@ -69,10 +69,10 @@ ALLOWED_HOSTS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get('CLIENT_ORIGIN'),
+    os.environ.get('CLIENT_ORIGIN')
 ]
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
