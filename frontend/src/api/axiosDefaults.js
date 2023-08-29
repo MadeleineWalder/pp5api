4c098daa50022f7dfdb06fd640aa5eb0c68cfebe
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-08-29 13:05:52 +0000

    adding baseURL in axiosDefaults

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,15 +1,6 @@
-// IMPORTANT!!
-// Because this React app is running in the same workspace as the API,
-
-// there is no need to set a separate baseURL until you reach deployment.
-
-// Setting a baseURL before you reach deployment will cause errors
-
-// Moments > Nav & Auth > Auth: Creating the SignUp form -Part 1
-
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://pp5api-a60f85b616aa.herokuapp.com/";
+axios.defaults.baseURL = "api/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
